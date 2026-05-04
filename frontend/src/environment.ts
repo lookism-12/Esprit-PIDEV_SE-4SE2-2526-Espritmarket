104abe5c092b9374fc88a6ace50ac2dfff95cc29
lookism-12 <137060350+lookism-12@users.noreply.github.com>
2026-05-04 14:56:44 +0100

    Switch environment to production mode

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://marketplace-backend-czeeenabcxb2djfu.spaincentral-01.azurewebsites.net/api
-'
+  production: true,
+  apiUrl: 'https://marketplace-backend-czeeenabcxb2djfu.spaincentral-01.azurewebsites.net/api'
 };
