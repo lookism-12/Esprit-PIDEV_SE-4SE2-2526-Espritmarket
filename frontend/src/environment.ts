2d921d628a2b777ab7ade1f9ce3c2b5aca27a60e
lookism-12 <137060350+lookism-12@users.noreply.github.com>
2026-05-04 14:47:41 +0100

    Update apiUrl to include 'https://' and endpoint

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'marketplace-backend-czeeenabcxb2djfu.spaincentral-01.azurewebsites.net'
+  apiUrl: 'https://marketplace-backend-czeeenabcxb2djfu.spaincentral-01.azurewebsites.net/api
+'
 };
